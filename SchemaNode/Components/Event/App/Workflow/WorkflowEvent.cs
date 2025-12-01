b8b5e3a659d7339aaa8b47cd322969ac364e0623
kurapica <kurapica125@outlook.com>
2025-12-01 23:04:08 +0800

    temp

---
+++ SchemaNode/Components/Event/App/Workflow/WorkflowEvent.cs
@@ -0,0 +1,11 @@
+﻿using SchemaNode.Context;
+
+namespace SchemaNode.Components;
+
+public class WorkflowEvent(WorkflowContext context): AppEvent(context.Workflow!.App)
+{
+    /// <summary>
+    /// The topic
+    /// </summary>
+    public override string Topic => $"{base.Topic}/{context.Workflow.Name}";
+}
