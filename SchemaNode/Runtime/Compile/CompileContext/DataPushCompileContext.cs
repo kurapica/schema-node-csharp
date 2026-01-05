90b6af6b995f8803610c7bfdefcea38e79bd657e
kurapica <kurapica125@outlook.com>
2026-01-06 00:59:20 +0800

    Update DataPushCompileContext.cs

--- SchemaNode/Runtime/Compile/CompileContext/DataPushCompileContext.cs
+++ SchemaNode/Runtime/Compile/CompileContext/DataPushCompileContext.cs
@@ -1,4 +1,3 @@
-using System.Linq.Expressions;
 using SchemaNode.Context;
 using SchemaNode.Enum;
 using SchemaNode.Function;
@@ -31,14 +30,11 @@ public class DataPushCompileContext(SchemaContext context, FunctionType funcType
         for(int i = 0; i < structExp.Fields.Length; i++)
         {
             StructFieldExpression field = structExp.Fields[i];
-            
-            // Check the field if related to third app field
-            bool isThirdRelated = CheckThirdRelation(field.Expression);
-            
+                        
             // only primary key field could be third app field related, if not the push data design is wrong
             if (primaryKeys.Contains(field.Name))
             {
-                if (isThirdRelated)
+                if (CheckThirdField(field.Expression))
                 {
                     changed = true;
                 }
@@ -50,7 +46,7 @@ public class DataPushCompileContext(SchemaContext context, FunctionType funcType
             else
             {
                 // if related to thrid app field, fail the visit
-                if (isThirdRelated)
+                if (!AvoidThirdField(field.Expression))
                     throw new FunctionVisitException(SchemaNodeStatus.ApplicationPushDataWrongFunc, APP_PUSH_DATA_WRONG_FUNC);
                 
                 fields[i] = field;
@@ -60,15 +56,18 @@ public class DataPushCompileContext(SchemaContext context, FunctionType funcType
         // No third app field related, used it directly
         if (!changed) return schema;
     }
-    
-    bool CheckThirdRelation(SchemaExpression expression)
+
+    /// <summary>
+    /// Check if require third app field
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <returns></returns>
+    bool CheckThirdField(SchemaExpression expression)
     {
         switch (expression)
         {
             case VariableExpression varExp:
-                bool result = CheckThirdRelation(varExp.Value);
-                if (result) removeExps.Add(varExp.Name);
-                return result;
+                break;
             
             case ArgumentExpression argExp:
                 break;
@@ -93,6 +92,16 @@ public class DataPushCompileContext(SchemaContext context, FunctionType funcType
                         break;
                     case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappfdatabyfourkey)}":
                         break;
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdata)}":
+                        break;
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabyonekey)}":
+                        break;
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabytwokey)}":
+                        break;
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabythreekey)}":
+                        break;
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabyfourkey)}":
+                        break;
                 }
                 break;
             case UnaryLogicExpression unaryLogicExp:
@@ -115,6 +124,82 @@ public class DataPushCompileContext(SchemaContext context, FunctionType funcType
                 break;
             case BinaryArithmeticExpression binaryArithmeticExp:
                 break;
+        }
+    }
+
+    /// <summary>
+    /// No third app field involved
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <returns></returns>
+    bool NoThirdField(SchemaExpression expression)
+    {
+        switch (expression)
+        {
+            case VariableExpression varExp:
+                return NoThirdField(varExp.Value);
+
+            case ArgumentExpression:
+            case ConstantExpression:
+            case NullExpression:
+                return true;
+
+            case DefaultExpression defExp:
+                return NoThirdField(defExp.Inner);
+
+            case ParamsExpression paramsExp:
+                return paramsExp.Exps.All(NoThirdField);
+
+            case IteratorExpression iterExp:
+                return NoThirdField(iterExp.Array);
+
+            case FuncCallExpression funcCallExp:
+                // @TODO: more check
+                switch (funcCallExp.Function.Name)
+                {
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappfdata)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappfdatabyonekey)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappfdatabythreekey)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappfdatabyfourkey)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdata)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabyonekey)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabytwokey)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabythreekey)}":
+                    case $"{NS_SYSTEM_DATA}.{nameof(SystemData.getappdatabyfourkey)}":
+                        return false;
+                }
+                return true;
+            case UnaryLogicExpression unaryLogicExp:
+                return NoThirdField(unaryLogicExp.Inner);
+
+            case BinaryLogicExpression binaryLogicExp:
+                return NoThirdField(binaryLogicExp.Left) && NoThirdField(binaryLogicExp.Right);
+
+            case ConditionalExpression condExp:
+                return NoThirdField(condExp.Condition) && NoThirdField(condExp.TrueExp) && NoThirdField(condExp.FalseExp);
+
+            case FieldAccessExpression fieldAccessExp:
+                return NoThirdField(fieldAccessExp.Owner);
+
+            case DataSourceExpression dataSourceExp:
+            case DataResultExpression dataResultExp:
+                return false;
+
+            case CollectionExpression collectionExp:
+                return NoThirdField(collectionExp.Iterator) && NoThirdField(collectionExp.Loop);
+
+            case BreakExpression breakExp:
+                // No break exp in data push
+                throw new FunctionVisitException(SchemaNodeStatus.ApplicationPushDataWrongFunc, APP_PUSH_DATA_WRONG_FUNC);
+
+            case UnaryArithmeticExpression unaryArithmeticExp:
+                return NoThirdField(unaryArithmeticExp.Inner);
+
+            case BinaryArithmeticExpression binaryArithmeticExp:
+                return NoThirdField(binaryArithmeticExp.Left) && NoThirdField(binaryArithmeticExp.Right);
+
+            default:
+                return true;
         }
     }
 }
