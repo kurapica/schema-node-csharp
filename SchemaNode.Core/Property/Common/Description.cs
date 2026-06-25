15e18469858c5913b7873a82e8845ff579e613ad
kurapica <kurapica125@outlook.com>
2026-06-25 22:07:16 +0800

    Update Description.cs

--- SchemaNode.Core/Property/Common/Description.cs
+++ SchemaNode.Core/Property/Common/Description.cs
@@ -9,7 +9,7 @@ namespace SchemaNode.Property.Common;
 /// <summary>
 /// The description property, which is used to specify the description information
 /// </summary>
-[Meta<ForSchema>(SCHEMA_KIND_NODE, SCHEMA_KIND_STRUCT_FIELD, SCHEMA_KIND_ENUM_VALUE)]
+[Meta<ForSchema>(SCHEMA_KIND_NODE, SCHEMA_KIND_STRUCT_FIELD)]
 [Meta<OfSchema>(SCHEMA_KIND_PROPERTY)]
 [Meta<SchemaType>($"{NS_SYSTEM_SCHEMA_PROPERTY_COMMON}.{nameof(Description)}")]
 public class Description : Property<LocaleString>;
