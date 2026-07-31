ad85daee88987e490316d13689400340c78a3e9e
kurapica <kurapica125@outlook.com>
2026-07-31 16:53:22 +0800

    Update EntrySource.cs

--- SchemaNode.Core/Property/Core/EntrySource.cs
+++ SchemaNode.Core/Property/Core/EntrySource.cs
@@ -7,5 +7,5 @@ namespace SchemaNode.Property.Core;
 [Meta<ForSchema>(SCHEMA_KIND_INT, SCHEMA_KIND_DECIMAL, SCHEMA_KIND_STRING)]
 [Meta<OfSchema>(SCHEMA_KIND_PROPERTY)]
 [Meta<SchemaType>($"{NS_SYSTEM_SCHEMA_PROPERTY_CORE}.entrysource")]
-[Relation<Valid, Relation.Assign>($"{nameof(EntrySource)}.{nameof(FuncCall.Func)}", NS_SYSTEM_SCHEMA_REFLECT_FUNC_WITH_RETURN, NODE_SELF, $"{NS_SYSTEM_LIST}<{NS_SYSTEM_ENTRY}>", true)]
+[Relation<Valid, Relation.Assign>($"{nameof(EntrySource)}.{nameof(FuncCall.Func)}", NS_SYSTEM_SCHEMA_REFLECT_FUNC_WITH_RETURN, NODE_SELF, $"{NS_SYSTEM_LIST}<{NS_SYSTEM_ENTRY_ACCESS}>", true)]
 public class EntrySource : FuncCallProperty;
